259d91092e5d3d784d98b44cb3011a31998df26c
Thrigger <david.thiringer93@gmail.com>
2020-02-20 20:34:34 +0100

    added some algo clearification

--- main.py
+++ main.py
@@ -20,12 +20,19 @@ e_path = 'inputs/e_so_many_books.txt'
 f_path = 'inputs/f_libraries_of_the_world.txt'
 num_books, num_libs, num_days, book_scores, libraries, booksets_for_libs = read(a_path)
 
+output_list_of_libs[]
+
 current_days = 0
 while num_days > current_day:
-    #loop each lib in lib list
-        #call function to calculate how much score this lib can generate on given days (days-current_day)
-        #if highest score yet, store the index of lib
-    #the index of the lib with highest score is added to order list
+    highest_score_yet = 0;
+    highest_i = 0
+    for i, lib in enumerte(libs):
+        temp_scores = get_score_from_lib(lib, num_days-current_day)
+        if temp_score >= highest_score_yet:
+            highest_i = i
+            highest_score_yet= temp_score
+
+    output_list_of_libs.append(i)
     #purge books from other libs 
     #set lib to used
     current_day += 1 #todo lib.days_toscan
