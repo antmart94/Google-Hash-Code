87dd5eb730653aefe9a92cc134ee6bde7d3991bc
Thrigger <david.thiringer93@gmail.com>
2020-02-20 20:24:42 +0100

    added pseudocode for algo

--- main.py
+++ main.py
@@ -11,28 +11,6 @@ import Lib
 import Book
 import numpy
 from utils import read
-"""
-def convert_to_int(line):
-    return [int(i) for i in line.rstrip().split(' ')]
-
-def read(path):
-    with open(path, 'r') as f:
-        num_books, num_libs, num_days = convert_to_int(f.readline()) # num_books, num_libs, num_days 
-        # B section
-        scores = convert_to_int(f.readline()) # Individual book scores
-        # L section
-        libs = []
-        booksets_in_lib = []
-        while True:
-            line1 = f.readline()
-            line2 = f.readline()
-            if not line1 or not line2: break
-            
-            libs.append(convert_to_int(line1)) # num_books in library, signup time, num book shipped 
-            booksets_in_lib.append(convert_to_int(line2))
-            
-    return num_books, num_libs, num_days, scores, libs, booksets_in_lib
-    """
 
 a_path = 'inputs/a_example.txt'
 b_path = 'inputs/b_read_on.txt'
@@ -42,11 +20,14 @@ e_path = 'inputs/e_so_many_books.txt'
 f_path = 'inputs/f_libraries_of_the_world.txt'
 num_books, num_libs, num_days, book_scores, libraries, booksets_for_libs = read(a_path)
 
-#for n_books, days, 
-#L1 = Lib() #libId, bookSet,  signUp,  scan
-#B1 = Book() # bId, score
+current_days = 0
+while num_days > current_day:
+    #loop each lib in lib list
+        #call function to calculate how much score this lib can generate on given days (days-current_day)
+        #if highest score yet, store the index of lib
+    #the index of the lib with highest score is added to order list
+    #purge books from other libs 
+    #set lib to used
+    current_day += 1 #todo lib.days_toscan
 
 
-
-#while days > current_day:
-#    get_opt_score(lib, book,)
