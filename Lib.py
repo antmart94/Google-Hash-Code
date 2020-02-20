1b0e81af9dbe379f4d1beb3409b9d89c08034ab9
Erik Lundin <erikjohanlundin@gmail.com>
2020-02-20 21:04:56 +0100

    modifed lib

--- Lib.py
+++ Lib.py
@@ -2,17 +2,21 @@
 
 import pandas as pd
 import re
+import numpy as np
 
 class Lib:
 
-    def __init__(self,  libId, bookSet,  signUp,  scan):
-            self.libId = libId
-            self.bookSet = bookSet
-            self.signUp = signUp
-            self.scan = scan
-            self.is_used = 0
+    def __init__(self,  libId, bookSet,  signUp,  scan, all_books):
+        self.libId = libId
+        self.bookSet = bookSet
+        self.signUp = signUp
+        self.scan = scan
+        self.is_used = 0
+        #self.books = [b for b in books if b.bid in bookSet]
 
-    def remove_from_book_set(book_id_to_remove):
-        if book_id_to_remove in bookSet:
-            bookSet.remove(book_id_to_remove)
+    def remove_from_book_set(self, book_id_to_remove):
+        if book_id_to_remove in self.bookSet:
+            self.bookSet.remove(book_id_to_remove)
 
+    def get_score_from_lib(self):
+        return 5
