a53c190e2c2ebaf845387afb8f879f2d0d5890ab
Anton Martinsson <acfm@Antons-MacBook-Pro.local>
2020-02-20 19:10:56 +0100

    Adding main and Reader

---
+++ Reader.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 19:09:38 2020
+
+@author: acfm
+"""
+
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 18:40:52 2020
+
+@author: acfm
+"""
+
+import pandas as pd
+import re
